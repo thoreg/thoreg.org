1cf0d708a86f888c26583063471b520d072b1b14
Thomas Rega <mail@thoreg.org>
2013-11-23 22:32:58 +0100

    First initial commit

---
+++ wsgi.py
@@ -0,0 +1,18 @@
+
+import os
+import sys
+
+paths = [
+    '/srv/w3/',
+]
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
